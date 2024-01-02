a3eb54fb736a585e4034d78112231ac1b6628e2a
CozDung29 <codung2909@gmail.com>
2024-01-02 13:13:03 +0700

    updated

--- controllers/productController.js
+++ controllers/productController.js
@@ -45,30 +45,54 @@ class ProductController {
     //     }
     // }
 
+    // async updateProduct(req, res, next) {
+    //     try {
+    //       const productId = req.params.productId;
+    //       const product = req.body;
+      
+    //       if (productId) {
+    //         // Kiểm tra xem sản phẩm có tồn tại không
+    //         const existingProduct = await ProductModel.getById(productId);
+      
+    //         // Cập nhật sản phẩm nếu nó tồn tại
+    //         if (existingProduct) {
+    //           await ProductModel.updateProduct(productId, product);
+    //           res.json(product); // Trả về sản phẩm đã cập nhật
+    //         } else {
+    //           res.status(404).json({ message: "Product not found" });
+    //         }
+    //       } else {
+    //         res.status(400).json({ message: "Product ID is required" });
+    //       }
+    //     } catch (err) {
+    //       console.error(err);
+    //       res.status(500).json({ message: "Internal server error" });
+    //     }
+    //   }
+
     async updateProduct(req, res, next) {
-        try {
-          const productId = req.params.productId;
-          const product = req.body;
-      
-          if (productId) {
-            // Kiểm tra xem sản phẩm có tồn tại không
-            const existingProduct = await ProductModel.getById(productId);
-      
-            // Cập nhật sản phẩm nếu nó tồn tại
+      const productId = req.params.productId;
+      const product = req.body;
+    
+      if (productId) {
+        ProductModel.getById(productId)
+          .then(existingProduct => {
             if (existingProduct) {
-              await ProductModel.updateProduct(productId, product);
-              res.json(product); // Trả về sản phẩm đã cập nhật
+              return ProductModel.updateProduct(productId, product);
             } else {
-              res.status(404).json({ message: "Product not found" });
+              return res.status(404).json({ message: "Product not found" });
             }
-          } else {
-            res.status(400).json({ message: "Product ID is required" });
-          }
-        } catch (err) {
-          console.error(err);
-          res.status(500).json({ message: "Internal server error" });
-        }
+          })
+          .then(() => res.json(product)) // Trả về sản phẩm đã cập nhật
+          .catch(err => {
+            console.error(err);
+            res.status(500).json({ message: "Internal server error" });
+          });
+      } else {
+        res.status(400).json({ message: "Product ID is required" });
       }
+    }
+    
 
 }
 
